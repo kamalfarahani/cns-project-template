b9f3405d1c14fc38801c861d216ae0b333c6dcca
Kamal (Riwogo) <pmer93@gmail.com>
2021-04-10 16:28:43 +0430

    implement plotting functions for LIFPopulation

--- cnsproject/plotting/plotting.py
+++ cnsproject/plotting/plotting.py
@@ -1,15 +1,51 @@
 """
 Module for visualization and plotting.
-
-TODO.
-
-Implement this module in any way you are comfortable with. You are free to use\
-any visualization library you like. Providing live plotting and animations is\
-also a bonus. The visualizations you will definitely need are as follows:
-
-1. F-I curve.
-2. Voltage/current dynamic through time.
-3. Raster plot of spikes in a neural population.
-4. Convolutional weight demonstration.
-5. Weight change through time.
 """
+import matplotlib.pyplot as plt
+from typing import List, Callable
+
+from ..network.neural_populations import LIFPopulation
+
+
+def plot_lif(lif: LIFPopulation, potentials: List[float]):
+    steps = len(potentials)
+    potential_times = [i * lif.dt for i in range(steps)]
+    plt.plot(potential_times, potentials, c='g')
+    plt.scatter(
+        lif.spike_times,
+        [lif.threshold for _ in range(len(lif.spike_times))],
+        c='r',
+        label='spikes')
+
+    plt.xlabel('Time')
+    plt.ylabel('Potential')
+    plt.legend()
+    plt.show()
+
+
+def plot_current(current: Callable[[float], float], steps: int, dt: float):
+    times = [dt * i for i in range(steps)]
+    current_values = [current(dt * i) for i in range(steps)]
+    plt.plot(times, current_values)
+    plt.xlabel('Time')
+    plt.ylabel('Current')
+    plt.show()
+
+
+def plot_FI_for_lif(lif: LIFPopulation, simulation_seconds=5, current_range=50):
+    freqs = []
+    steps = int(simulation_seconds / lif.dt)
+    for i in range(current_range):
+        lif.refractory_and_reset()
+        lif.spike_times = []
+        lif.current = lambda t: i
+        for _ in range(steps):
+            lif.forward(None)
+        
+        frequency = len(lif.spike_times) / simulation_seconds
+        freqs.append(frequency)
+
+    plt.plot([i for i in range(current_range)], freqs)
+    plt.xlabel('I(t)')
+    plt.ylabel('Frequency')
+    plt.show()
