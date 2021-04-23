5f9573e93ccbc29c9bfbe4db1b3136ab92eae70b
Kamal (Riwogo) <pmer93@gmail.com>
2021-04-23 11:43:26 +0430

    remove commented codes

--- cnsproject/plotting/plotting.py
+++ cnsproject/plotting/plotting.py
@@ -29,6 +29,7 @@ def get_spiked_neurons(spikes: torch.Tensor) -> torch.Tensor:
     
     return torch.tensor(spiked_neurons)
 
+
 def plot_activity(populatins_spikes: List[torch.Tensor], dt: float):
     steps = len(populatins_spikes[0])
     population_size = reduce(lambda acc, pop: acc + len(pop[0]), populatins_spikes, 0)
@@ -66,22 +67,6 @@ def raster_plot(populatins_spikes: List[torch.Tensor], dt: float):
     plt.xlabel('Time')
     plt.ylabel('Raster Activity')
     plt.show()
-    
-
-# def plot_neuron_potential(lif: LIFPopulation, potentials: List[float]) -> None:
-#     steps = len(potentials)
-#     potential_times = [i * lif.dt for i in range(steps)]
-#     plt.plot(potential_times, potentials, c='g')
-#     plt.scatter(
-#         lif.spike_times,
-#         [lif.threshold for _ in range(len(lif.spike_times))],
-#         c='r',
-#         label='spikes')
-
-#     plt.xlabel('Time')
-#     plt.ylabel('Potential')
-#     plt.legend()
-#     plt.show()
 
 
 def plot_current(currents: torch.Tensor, steps: int, dt: float) -> None:
@@ -91,24 +76,6 @@ def plot_current(currents: torch.Tensor, steps: int, dt: float) -> None:
     plt.ylabel('Current')
     plt.show()
 
-
-# def plot_FI_for_neuron(lif: LIFPopulation, simulation_seconds=5, current_range=50) -> None:
-#     freqs = []
-#     steps = int(simulation_seconds / lif.dt)
-#     for i in range(current_range):
-#         lif.refractory_and_reset()
-#         lif.spike_times = []
-#         lif.current = lambda t: i
-#         for _ in range(steps):
-#             lif.forward(None)
-        
-#         frequency = len(lif.spike_times) / simulation_seconds
-#         freqs.append(frequency)
-
-#     plt.plot([i for i in range(current_range)], freqs)
-#     plt.xlabel('I(t)')
-#     plt.ylabel('Frequency')
-#     plt.show()
 
 def plot_adaptation(adaptations: List[float], dt):
     times = [dt * i for i in range(len(adaptations))]
