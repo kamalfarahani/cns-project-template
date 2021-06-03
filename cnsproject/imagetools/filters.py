a1184a3bd7d0f88ea2a3a35f44e9256d4f6378c6
Kamal (Riwogo) <pmer93@gmail.com>
2021-06-03 14:40:32 +0430

    add DoG and gabor filters

---
+++ cnsproject/imagetools/filters.py
@@ -0,0 +1,57 @@
+import torch
+
+from typing import Tuple
+from numpy import pi, sqrt, cos, sin
+
+
+def make_axis(size: int) -> Tuple[torch.Tensor, torch.Tensor]:
+    n = int((size - 1) / 2) 
+    axis_range = torch.arange(-n, n + 1)
+    xs, ys = torch.meshgrid([axis_range, axis_range])
+
+    return (xs, ys)
+
+
+def gaussian(xs: torch.Tensor, ys: torch.Tensor, std: float) -> torch.Tensor:
+    return torch.exp(-(xs ** 2 + ys ** 2) / (2 * std)) / (std * sqrt(2 * pi))
+
+
+def make_gaussian_filter(size: int, std: float):
+    xs, ys = make_axis(size)
+    
+    return gaussian(xs, ys, std)
+
+
+def DoG(std_1: float, std_2: float, size: int) -> torch.Tensor:
+    g1 = make_gaussian_filter(size, std_1)
+    g2 = make_gaussian_filter(size, std_2)
+
+    return (g1 - g2)
+
+
+def gabor(lambda_: float, theta: float, sigma: float, gamma: float, size: int) -> torch.Tensor:
+    xs, ys = make_axis(size)
+    X = xs * cos(theta) + ys * sin(theta)
+    Y = -xs * sin(theta) + ys * cos(theta)
+    
+    E = torch.exp(-(X ** 2 + gamma ** 2 * Y ** 2) / (2 * sigma ** 2))
+    C = torch.cos((2 * pi / lambda_) * X)
+
+    return (E * C)
+
+
+def convolve(image: torch.Tensor, filter: torch.Tensor):
+    image_rows = image.shape[0]
+    image_columns = image.shape[1]
+    
+    filter_rows = filter.shape[0]
+    filter_columns = filter.shape[1]
+    
+    result = torch.zeros(image_rows - filter_rows + 1, image_columns - filter_columns + 1)
+    rows = result.shape[0]
+    columns = result.shape[1]
+    for i in range(rows):
+        for j in range(columns):
+            result[i, j] = (filter * image[i: i + filter_rows, j: j + filter_columns]).sum()
+    
+    return result.clamp(min=0, max=256)
